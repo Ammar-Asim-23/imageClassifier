e87658bb51595f15208f5e77f9f823dc70f8455b
Ammar-Asim-23 <ammarasim065@gmail.com>
2023-08-24 13:28:04 +0500

    feat: added frontend and connected to Flask app

--- Project/app.py
+++ Project/app.py
@@ -1,34 +1,45 @@
-from flask import Flask, render_template, request, redirect, jsonify
-from tensorflow.keras.models import load_model
-import PIL
 import numpy as np
+import PIL.Image
+from flask import Flask, request, jsonify, render_template
+from tensorflow.keras.models import load_model
 
-
-#Initialize flask app
 app = Flask(__name__)
 
-#load your pretrained model
+# Load the pretrained CNN model
 model = load_model('imageClassifier/Project/models/CNN.h5')
 
+# Preprocess the image using the provided load_image function
+def load_image(image):
+    img = PIL.Image.open(image)
+    img = np.array(img)
+    img = img.reshape(-1, 28, 28, 1)
+    return img
+
+# Predict digit using the provided predict_digit function
 def predict_digit(digit_image):
-    #reshape image
-    digit_image = digit_image.reshape(-1, 28, 28, 1)
-    
-    #predict digit using model
     pred_digit = model.predict(digit_image)
-    
     return pred_digit.argmax()
 
-def load_image(image):
-    #load image
-    img = PIL.Image.open(image)
+@app.route('/predict', methods=['POST'])
+def predict_digit_endpoint():
+    try:
+        # Get the image file from the request
+        image_file = request.files['image']
+        # Load the image using the load_image function
+        processed_image = load_image(image_file)
+        # Predict the digit using the predict_digit function
+        predicted_digit = predict_digit(processed_image)
+        
+        response = {'predicted_digit': int(predicted_digit)}
+        return jsonify(response)
     
-    
-    #convert image to array
-    img = (np.array(img))
-    
-    #reshaping to support our model input and normalizing
-    img = img.reshape(-1, 28, 28, 1)
-    
-    return img    
-print(model.predict((np.log1p(np.array(load_image('imageClassifier/Project/saved_images/image_431.png'))))).argmax())
+    except Exception as e:
+        error_response = {'error': str(e)}
+        return jsonify(error_response), 400
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
